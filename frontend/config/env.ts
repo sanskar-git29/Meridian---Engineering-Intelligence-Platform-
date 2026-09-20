42d097b521fdd1a843fed1c78c03f4303b76cc7b
Sanskar Kumar <sanskarkumar7536@gmail.com>
2026-09-20 23:49:47 +0530

    feat: create env configuration file and update axios baseURL

---
+++ frontend/config/env.ts
@@ -0,0 +1,6 @@
+ const env={
+    NEXT_PUBLIC_API_BASE_URL: String(process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000"),
+
+ }
+
+ export default env;
--- frontend/lib/api/axios.ts
+++ frontend/lib/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+import env from "@/config/env.js";
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000",
+  baseURL: env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
